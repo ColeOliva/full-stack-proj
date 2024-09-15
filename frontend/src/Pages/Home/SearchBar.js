c191af789f4d5da9b851f8108899394dc8ef3c0e
Cole Oliva <olivac2@wwu.edu>
2024-09-15 08:29:04 -0700

    touching up searchbar

--- frontend/src/Pages/Home/SearchBar.js
+++ frontend/src/Pages/Home/SearchBar.js
@@ -21,41 +21,66 @@ const SearchBar = ({ searchUser, information }) => {
 
   return (
     <InputForm onSubmit={handleSubmit} info={information}>
-      <Button type="submit">
-        <IoSearchOutline size={20} color={"blue"} />
+      <div className="leftSideSearch">
+        <Button type="submit">
+          <IoSearchOutline size={20} color={"blue"} />
+        </Button>
+        <Input
+          className="searchInput"
+          value={text}
+          type="text"
+          onChange={handleChange}
+          placeholder="Search Github Username..."
+        />
+      </div>
+      <Button className="searchButton" type="submit">
+        Search
       </Button>
-      <Input
-        value={text}
-        type="text"
-        onChange={handleChange}
-        placeholder="Search for any user..."
-      />
     </InputForm>
   );
 };
 
 const InputForm = styled.form`
+  height: 50px;
   display: flex;
   justify-content: space-between;
   align-items: center;
   background-color: ${Color.backgroundWhite};
   padding: 1rem;
   border-radius: 8px;
-`;
-/* 
-  &:focus-within {
-    box-shadow: 0 0 0 2px
-      ${(props) => {
-        if (!props.information) return Color.error;
-        else return Color.backgroundGray;
-      }}; // props
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  font-family: "Space Mono", monospace;
+  font-weight: ${Color.fontWeight};
+
+  .searchInput {
+    width: 75vw;
   }
-`; */
+
+  .leftSideSearch {
+    display: flex;
+    align-items: center;
+  }
+
+  .searchButton {
+    background-color: ${Color.primaryButton};
+    font-family: "Space Mono", monospace;
+    color: ${Color.backgroundWhite};
+    border-radius: 8px;
+    padding: 16px;
+    font-size: 16px;
+    width: 100px;
+    height: 50px;
+    border: none;
+    cursor: pointer;
+  }
+`;
 
 const Input = styled.input`
   border: none;
   outline: none;
   background-color: inherit;
+  font-family: "Space Mono", monospace;
+  font-weight: ${Color.fontWeight};
 `;
 
 const Button = styled.button`
